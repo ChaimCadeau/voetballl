3da00d1e73f80c87ceb50c8add2cebc3ff1a5c16
ChaimCadeau <chaim.cadeau1@student.rocva.nl>
2024-05-22 20:25:25 +0200

    aanpasigen

--- contact.php
+++ contact.php
@@ -1,1 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contact</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="contact-box">
+    <form>
+        <label>
+            <input type="text" class="input-field" placeholder="Your Name">
+        </label>
+        <label>
+            <input type="email" class="input-field" placeholder="Your Email">
+        </label>
+        <label>
+            <input type="text" class="input-field" placeholder="subject">
+        </label>
+        <label>
+            <textarea type="text" class="input-field textarea-field" Your Message=""></textarea>
+        </label>
+        <button type="button" class="btn">Send Message</button>
+    </form>
+</div>
+
+</body>
+</html>
